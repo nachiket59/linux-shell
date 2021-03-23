b86c0b262fd80bc8dd5459a36de82418726252bc
nachiket59 <pethenachiket5@gmail.com>
2021-03-23 13:39:47 +0530

    fg bg changes

--- shell.c
+++ shell.c
@@ -8,6 +8,7 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <signal.h>
+
 #define LINE_SIZE 100
 #define NO_OF_ARGS 100
 #define NO_OF_PIPES 100
@@ -22,23 +23,46 @@ struct child_processes{
 	int cur_bg;
 }children; 
 
-int process_exists(int *arr, int proc, int n){
+void print_jobs(){
+	if(children.cur_stopped < 0){
+		printf("No stopped jobs.\n");
+	}
+	int i;
+	printf("\n");
+	for(i=0; i<=children.cur_stopped;i++){
+		printf("%d \n",children.stopped[i] );
+	}
+}
+
+int get_process_index(int *arr, int proc, int n){
 	int i;
 	for(i = 0; i<=n ;i++){
 		if(arr[i] == proc){
-			return 1;
+			return i;
 		}
 	}
-	return 0;
+	return -1;
+}
+
+void shift_left(int *arr, int n, int k){
+	int i;
+	for(i = k; i<n; i++){
+		arr[i] = arr[i+1];
+	}
+	return;
 }
 
 // returns 1 if some job was brought in fg else 0
 int handle_fg(char* pid){
+	int i = 0;
+	
 	int p = atoi(pid);
-	if(process_exists(&children.stopped, p, children.cur_stopped)){
+	int index = get_process_index(&children.stopped, p, children.cur_stopped);
+	if(index != -1){
 		kill(p,SIGCONT);
 		children.cur_running ++;
-		children.running[children.cur_running] = children.stopped[children.cur_stopped];
+		children.running[children.cur_running] = children.stopped[index];
+		shift_left(&children.stopped, children.cur_stopped, index);
 		children.cur_stopped --;
 		//printf("%d fg \n",children.cur_stopped );
 		return 1;
@@ -47,14 +71,17 @@ int handle_fg(char* pid){
 		printf("process %d is not stopped or doest exist.\n",p );
 		return 0;
 	}
+	
 }
 
 int handle_bg(char* pid){
 	int p = atoi(pid);
-	if(process_exists(&children.stopped, p, children.cur_stopped)){
+	int index = get_process_index(&children.stopped, p, children.cur_stopped);
+	if(index != -1){
 		kill(p,SIGCONT);
 		children.cur_bg ++;
-		children.bg[children.cur_bg] = children.stopped[children.cur_stopped];
+		children.bg[children.cur_bg] = children.stopped[index];
+		shift_left(&children.stopped, children.cur_stopped, index);
 		children.cur_stopped --;
 		//printf("%d fg \n",children.cur_stopped );
 		return 1;
@@ -68,29 +95,37 @@ int handle_bg(char* pid){
 void ctrl_c_handler(){
 	int i, self_pid;
 	if(children.cur_running >= 0){
+		for(i = children.cur_running; i>=0 ; i--){
+			kill(children.running[i], SIGINT);
+		}
+	}
+
+	/*if(children.cur_running >= 0){
 		printf("%d\n",children.running[children.cur_running] );
 		kill(children.running[children.cur_running], SIGINT);
 		children.cur_running --;	
 		
-	}
-	else{
-		exit(1);
-	}	
+	}*/	
 }
 
 // to do -  do exactly what ctrl + z does, here fuction simply exits the shell.
 void ctrl_z_handler(){
 	int i;
-	if(children.cur_running >= 0){
+	for(i = children.cur_running; i>=0; i--){
+		printf("%d\n",children.running[i] );
+		kill(children.running[i], SIGSTOP);
+		children.cur_stopped ++;
+		children.stopped[children.cur_stopped] = children.running[i];
+		
+	}
+	children.cur_running = -1;
+	/*if(children.cur_running >= 0){
 		printf("%d\n",children.running[children.cur_running] );
 		kill(children.running[children.cur_running], SIGSTOP);
 		children.cur_stopped ++;
 		children.stopped[children.cur_stopped] = children.running[children.cur_running];
 		children.cur_running --;	
-	}
-	else{
-		kill(getpid(), SIGSTOP);
-	}
+	}*/
 }
 
 char * readline(){
@@ -306,126 +341,134 @@ int main() {
 	int pipe_cnt = 0;
 	int **pipes;
 	while(1) {
-			printf("nachiket's shell $");
-			cmd = readline();
-			if(strcmp(cmd,"") == 0){
-				continue;
+		printf("nachiket's shell $");
+		cmd = readline();
+		if(strcmp(cmd,"") == 0){
+			continue;
+		}
+		pipe_array = parse_pipes(cmd);
+		i = 1;
+		while(pipe_array[i]){
+			//printf("%s\n",pipe_array[i] );
+			i++;
+		}
+		pipe_cnt = i-1;
+		//printf("%d\n",pipe_cnt );
+		if(pipe_cnt > 0){
+			//initialise 2d array of size pipe_cnt
+			pipes = malloc(sizeof(int*) * pipe_cnt);
+			for(i = 0; i< pipe_cnt; i++){
+				pipes[i] = malloc(sizeof(int) *2);
 			}
-			pipe_array = parse_pipes(cmd);
-			i = 1;
-			while(pipe_array[i]){
-				//printf("%s\n",pipe_array[i] );
-				i++;
-			}
-			pipe_cnt = i-1;
-			//printf("%d\n",pipe_cnt );
-			if(pipe_cnt > 0){
-				//initialise 2d array of size pipe_cnt
-				pipes = malloc(sizeof(int*) * pipe_cnt);
-				for(i = 0; i< pipe_cnt; i++){
-					pipes[i] = malloc(sizeof(int) *2);
-				}
 
-				for(i = 0; i < pipe_cnt+1; i++){
-					pipe(pipes[i]);
-					cmd_array = parse_command(pipe_array[i]);
-					p = fork();
-					if(p == 0){
-						if(i == 0){
-							close(1);
-							dup(pipes[i][1]);
-							close(pipes[i][0]);
-							if(execvp(cmd_array[0], cmd_array) == -1){
-								perror("one");
-								exit(0);
-							}
+			for(i = 0; i < pipe_cnt+1; i++){
+				pipe(pipes[i]);
+				cmd_array = parse_command(pipe_array[i]);
+				p = fork();
+				if(p == 0){
+					if(i == 0){
+						close(1);
+						dup(pipes[i][1]);
+						close(pipes[i][0]);
+						if(execvp(cmd_array[0], cmd_array) == -1){
+							perror("one");
+							exit(0);
 						}
-						else if(i == pipe_cnt){
-							close(0);
-							dup(pipes[i-1][0]);
-							close(pipes[i-1][1]);
-							if(execvp(cmd_array[0], cmd_array) == -1){
-								perror("two");
-								exit(0);
-							}
-						}
-						else{
-							close(0);
-							dup(pipes[i-1][0]);
-							close(pipes[i-1][1]);
-							close(1);
-							dup(pipes[i][1]);
-							close(pipes[i][0]);
-							if(execvp(cmd_array[0], cmd_array) == -1){
-								perror("three");
-								exit(0);
-							}
+					}
+					else if(i == pipe_cnt){
+						close(0);
+						dup(pipes[i-1][0]);
+						close(pipes[i-1][1]);
+						if(execvp(cmd_array[0], cmd_array) == -1){
+							perror("two");
+							exit(0);
 						}
 					}
 					else{
-						children.cur_running += 1;
-						children.running[children.cur_running] = p;
-					}
-				}
-				waitpid(0,&wstatus,WUNTRACED);
-				children.cur_running = -1;
-				for(i = 0; i< pipe_cnt; i++){
-					free(pipes[i]);
-				}
-				free(pipes);
-			}
-			else{
-				//printf("here\n");
-				rd_array = parse_redirections(cmd);
-				cmd_array = parse_command(rd_array[0]);
-				if(strcmp(cmd_array[0],"fg") == 0){
-					if(handle_fg(cmd_array[1]))
-						waitpid(0,&wstatus,WUNTRACED);	
-					continue;
-				}
-				if(strcmp(cmd_array[0],"bg") == 0){
-					if(handle_bg(cmd_array[1]))
-						waitpid(0,&wstatus,WCONTINUED);	
-					continue;
-				}
-				p = fork();
-				if(p == 0) {
-					//printf("%s\n", rd_array[0]);
-					if(rd_array[1]){
-						printf("inr\n");
-						printf("%s\n",rd_array[1]);
 						close(0);
-						int fd = open(rd_array[1], O_RDONLY);
-						if(fd == -1){
-							printf("%d\n",errno);
-							exit(1);
-						}
-					}
-					if(rd_array[2]){
-						printf("out_r\n");
-						printf("%s\n",rd_array[2]);
+						dup(pipes[i-1][0]);
+						close(pipes[i-1][1]);
 						close(1);
-						int fd = open(rd_array[2], O_WRONLY | O_CREAT, 0777);
-						//printf("write %d\n", fd);
-						if(fd == -1){
-							printf("%d\n",errno);
-							exit(1);
+						dup(pipes[i][1]);
+						close(pipes[i][0]);
+						if(execvp(cmd_array[0], cmd_array) == -1){
+							perror("three");
+							exit(0);
 						}
-
 					}
-
-					if(execvp(cmd_array[0], cmd_array) == -1){
-						printf("undefined command %d\n",errno);
-						exit(0);
-					}
-				} 
-				else {
+				}
+				else{
 					children.cur_running += 1;
 					children.running[children.cur_running] = p;
-					waitpid(0,&wstatus,WUNTRACED);
-					children.cur_running = -1;
 				}
 			}
+			waitpid(0,&wstatus,WUNTRACED);
+			children.cur_running = -1;
+			for(i = 0; i< pipe_cnt; i++){
+				free(pipes[i]);
+			}
+			free(pipes);
+		}
+		else{
+			//printf("here\n");
+			rd_array = parse_redirections(cmd);
+			cmd_array = parse_command(rd_array[0]);
+			if(strcmp(cmd_array[0],"fg") == 0){
+				printf("\n");
+				if(handle_fg(cmd_array[1]))
+					waitpid(0,&wstatus,WUNTRACED);	
+				continue;
+			}
+			if(strcmp(cmd_array[0],"bg") == 0){
+				if(handle_bg(cmd_array[1]))
+					waitpid(0,&wstatus,WCONTINUED);	
+				continue;
+			}
+			if(strcmp(cmd_array[0],"jobs") == 0){
+				print_jobs();
+				continue;
+			}
+			if(strcmp(cmd_array[0],"exit") == 0){
+				exit(0);
+			}
+			p = fork();
+			if(p == 0) {
+				//printf("%s\n", rd_array[0]);
+				if(rd_array[1]){
+					//printf("inr\n");
+					//printf("%s\n",rd_array[1]);
+					close(0);
+					int fd = open(rd_array[1], O_RDONLY);
+					if(fd == -1){
+						printf("%d\n",errno);
+						exit(1);
+					}
+				}
+				if(rd_array[2]){
+					//printf("out_r\n");
+					//printf("%s\n",rd_array[2]);
+					close(1);
+					int fd = open(rd_array[2], O_WRONLY | O_CREAT, 0777);
+					//printf("write %d\n", fd);
+					if(fd == -1){
+						printf("%d\n",errno);
+						exit(1);
+					}
+
+				}
+
+				if(execvp(cmd_array[0], cmd_array) == -1){
+					printf("undefined command %d\n",errno);
+					exit(0);
+				}
+			} 
+			else {
+				children.cur_running += 1;
+				children.running[children.cur_running] = p;
+				waitpid(0,&wstatus,WUNTRACED);
+				children.cur_running = -1;
+			}
+		}
 	}
 	return 0;
 }
